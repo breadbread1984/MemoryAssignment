e09dc297aa59575ec692e7a1aefeb0f12be7f575
yi.xie <yi.xie@enflame-tech.com>
2022-06-26 21:15:02 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <boost/numeric/ublas/io.hpp>
 #include "memory.hpp"
 
 using namespace std;
+namespace ublas = boost::numeric::ublas;
 
 int main() {
   Memory memory(8);
   vector<vector<int> > tasks{{2,1,3},{2,2,5},{2,1,1},{1,1},{2,1},{1}};
   ublas::matrix<int> assignments = memory.assign(tasks);
+
 }
