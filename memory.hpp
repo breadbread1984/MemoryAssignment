7ff7b54b80acef8e483e4d51e4fbb6af41af8ff3
yi.xie <yi.xie@enflame-tech.com>
2022-08-07 10:34:45 +0800

    new data structure

--- memory.hpp
+++ memory.hpp
@@ -1,66 +1,47 @@
 #ifndef MEMORY_HPP
 #define MEMORY_HPP
 
-#include <iostream>
-#include <vector>
-#include <map>
-#include <iterator>
-#include <tuple>
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/matrix_proxy.hpp>
-#include <boost/numeric/ublas/io.hpp>
+#include <queue>
+#include <boost/multi_index_container.hpp>
 
 using namespace std;
-namespace ublas = boost::numeric::ublas;
+using namespace boost;
+using namespace boost::multi_index;
 
-class Counter: public iterator<forward_iterator_tag, vector<int>> {
-  int _base, _digits;
-  vector<int> _counter;
+struct Task {
+  int size;
+  int elapse;
+};
+
+struct Disposition {
+  int address;
+  int time;
+};
+
+struct IndexedByAddress {};
+struct IndexedByTime {};
+
+using Assignment = multi_index_container<
+  Disposition,
+  indexed_by<
+    ordered_non_unique<
+      tag<IndexedByAddress>,
+      member<Disposition, int, &Disposition::address>
+    >,
+    ordered_non_unique<
+      tag<IndexedByTime>,
+      member<Disposition, int, &DispositionL::time>
+    >
+  >
+>;
+
+class Allocator {
+  using Queue = queue<Assignment>;
 protected:
-  virtual void next();
+  int cost(const Assignment & cost);
 public:
-  Counter(int base, int digits);
-  Counter(const Counter& counter);
-  virtual ~Counter();
-  Counter& operator++();
-  Counter operator++(int);
-  bool operator==(const Counter& rhs) const;
-  bool operator!=(const Counter& rhs) const;
-  vector<int> & operator*();
-};
-
-class Assignment {
-  int _size;
-  map<int,tuple<int,int> > assignment; // address->(size, time)
-public:
-  Assignment(int size);
-  Assignment& operator=(const Assignment& a);
-  virtual ~Assignment();
-  bool append(int address, int size, int elapse);
-  tuple<int,int> address_range();
-  map<int,tuple<int,int> > & get();
-};
-
-class Assignments {
-  vector<Assignment> assignments;
-protected:
-  bool collide(int size, int elapse, int position, int time);
-public:
-  Assignments();
-  Assignments(const Assignments& a);
-  Assignments& operator=(const Assignments& a);
-  virtual ~Assignments();
-  bool append(Assignment assignment);
-  tuple<int,int> address_range();
-  vector<Assignment> & get();
-};
-
-class Memory {
-  int _size;
-public:
-  Memory(int size);
-  virtual ~Memory();
-  Assignments assign(const vector<vector<tuple<int,int> > > & tasks, int step = -1);
+  Allocator();
+  virtual ~Allocator();
 };
 
 #endif
