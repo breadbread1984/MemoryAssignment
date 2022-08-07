f65761f640fa7440f93524bd7620c3048db893c8
yi.xie <yi.xie@enflame-tech.com>
2022-08-07 11:08:19 +0800

    update

--- allocator.cpp
+++ allocator.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <limits>
 #include <algorithm>
 #include "allocator.hpp"
 
@@ -11,11 +12,18 @@ Allocator::~Allocator() {
 }
 
 int Allocator::cost(const Assignment & assignment, const vector<Task> & tasks) {
+  int min_value = numeric_limits<int>::max();
   for (auto itr = assignment.begin() ; itr != assignment.end() ; itr++) {
     if (itr->index >= tasks.size()) {
       throw logic_error("invalid task id!");
     }
+    int address = itr->address;
+    const Task & task = tasks[itr->index];
+    if (address + task.size < min_value) {
+      min_value = address + task.size;
+    }
   }
+  return min_value;
 }
 
 std::tuple<int, int> Allocator::bound(const Assignment & assignment, const Task & task) {
